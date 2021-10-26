398d201080b77922932ce70e98c7e011ea9f6c4e
lordupoun <240141@vutbr.cz>
2021-10-26 16:25:49 +0200

    test

---
+++ kalkulacka/kalkulacka.cpp
@@ -0,0 +1,35 @@
+﻿// kalkulacka.cpp : Defines the entry point for the application.
+//
+
+#include "kalkulacka.h"
+
+using namespace std;
+//zjistit jak udelat pole ktere ma definovanou velikost az v programu (po nacteni hodnot)
+//float misto int
+
+int main()
+{
+	int hodnoty[5][5];
+	for(int y = 0; y < 5; y++)
+	{ 
+		for (int x = 0; x < 5; x++)
+		{
+			scanf("%d", &hodnoty[x][y]);
+			//přidat reset funkci
+			printf(" ");
+		}
+		printf("\n");
+	}
+	//--------------------------------------
+	for (int y = 0; y < 5; y++) //vypis
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			printf("%5d", hodnoty[x][y]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
